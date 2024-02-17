b6afcc30d6eaac875607785935b4c08d96c2bb26
AlexandruCoca99 <114983771+AlexandruCoca99@users.noreply.github.com>
2024-02-17 14:56:37 +0200

    harta buna fara poze

--- public/functions.php
+++ public/functions.php
@@ -5,11 +5,14 @@ function get_includes()
     return '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript" src="../apis.google.com/js/plusone.js"></script>
     <script type="text/javascript" src="highslide/highslide-with-gallery.packed.js"/></script>
-    <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
-    <script src="https://cdn.amcharts.com/lib/5/map.js"></script>
-    <script src="https://cdn.amcharts.com/lib/5/geodata/worldLow.js"></script>
-    <script src="https://cdn.amcharts.com/lib/5/geodata/canadaLow.js"></script>
-    <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+    // <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+    // <script src="https://cdn.amcharts.com/lib/5/map.js"></script>
+    // <script src="https://cdn.amcharts.com/lib/5/geodata/worldLow.js"></script>
+    // <script src="https://cdn.amcharts.com/lib/5/geodata/canadaLow.js"></script>
+    // <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+    <script src="//cdn.amcharts.com/lib/4/core.js"></script>
+    <script src="//cdn.amcharts.com/lib/4/maps.js"></script>
+    <script src="//cdn.amcharts.com/lib/4/geodata/worldLow.js"></script>
     
     <link rel="stylesheet" type="text/css" href="highslide/highslide.css" />
     <link rel="stylesheet" type="text/css" href="css/reset.css" />
