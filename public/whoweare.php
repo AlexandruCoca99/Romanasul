002a9d515acd78f1a0d5ae5633939019004a6176
AlexandruCoca99 <114983771+AlexandruCoca99@users.noreply.github.com>
2024-06-13 13:18:47 +0300

    update who we are

--- public/whoweare.php
+++ public/whoweare.php
@@ -11,130 +11,132 @@ include 'header.php';
 </div>
     
 <div class="who-we-are-container">
-    <div class="mission-wrapper">
-        <h2 class="title">Mission:</h2>
-        <p>To disseminate and bring Romanian folklore on stage, to cultivate among students the taste for beauty and the passion for art.</p>
+    
+        <div class="mission-wrapper">
+            <h2 class="title">Mission:</h2>
+            <p>To disseminate and bring Romanian folklore on stage, to cultivate among students the taste for beauty and the passion for art.</p>
+        </div>
+    <div class="history-content-wrapper">
+        <div class="short-history-wrapper"> 
+            <h2 class="title">Short history:</h2>
+            <p>Founded in 1974, initiated by a group of students from the Polytechnic Institute of Cluj-Napoca, coordinated by prof. Teodor NILAS. Since 1979, we enjoy a fruitful collaboration with Prof. Dumitru POP, with the Language Faculty of the Cluj-Napoca Babes-Bolyai University. We have developed, attained maturity and achieved consecration in student's contests and festivals 'Primavara Studenteasca', 'Festivalul artei si creatiei studentesti', 'Festivalul National Studentesc de Folclor', in shows and tours both in our country and abroad. In 1984, we have starred in a movie, 'O ceata de calusari din Campia Transilvaniei', in 35mm, of certified artistic and documentary value. In 1994 and in 2000, we brought two monographies to print, by Prof. Teodor NILAS. We have made recordings at National Radio and TV stations, and we have recorded a CD.</span>
+        </div>
+    
+        <div class="short-history-images-container">
+            <img src="images/gallery/pozeVechi/1978_nasaud.JPG" alt="Nasaud-1978" class="photo-1">
+            <img src="images/gallery/pozeVechi/1978_ceterasi.JPG" alt="Ceterasi-1978" class="photo-2">
+        </div>
     </div>
+    <div class="members-container">
+        <div class="members-presentation">
+            <h2 class="title">Members:</h2>
+            <p>Students of the Technical University and other Universities of Cluj-Napoca: each year, more than 80 students participate in our activities. Dancers, singers and musicians. Conductors, choreographers, stage directors, referents, artistic counselors.</span>
+        </div>
+        <p>&nbsp;</p>
+        <p class="title">Choreographers:</p>
+        <span>
+            <ul type="circle">
+                <li>1. Bacsai Olga (1974 - 1975)</li>
+                <li>2. Onut Alexandru (1975 - 1976)</li>
+                <li>3. Miclaus Octavian (1976 - 1978)</li>
+                <li>4. Prof.Florin Ciobanu (1979 - 1994)</li>
+                <li>5. Ing.Viorel Roman (1994-2017)</li>
+                <li>5. Ing. Nicoara Eugen (2017-now)</li>
+            </ul>
+        </span>
 
-    <div class="short-history-wrapper"> 
-        <h2 class="title">Short history:</h2>
-        <p>Founded in 1974, initiated by a group of students from the Polytechnic Institute of Cluj-Napoca, coordinated by prof. Teodor NILAS. Since 1979, we enjoy a fruitful collaboration with Prof. Dumitru POP, with the Language Faculty of the Cluj-Napoca Babes-Bolyai University. We have developed, attained maturity and achieved consecration in student's contests and festivals 'Primavara Studenteasca', 'Festivalul artei si creatiei studentesti', 'Festivalul National Studentesc de Folclor', in shows and tours both in our country and abroad. In 1984, we have starred in a movie, 'O ceata de calusari din Campia Transilvaniei', in 35mm, of certified artistic and documentary value. In 1994 and in 2000, we brought two monographies to print, by Prof. Teodor NILAS. We have made recordings at National Radio and TV stations, and we have recorded a CD.</span>
+        <p>&nbsp;</p>
+        <p class="title">Conductors:</p>
+            <ul>
+                <li>1. Prof. Paul Stoica (1974 - 1975)</li>
+                <li>2. Iuliu Hoca (1975 - 1976)</li>
+                <li>3. Romică Râmbu (1975 - 1976)</li>
+                <li>4. Ştefan Lustig (1976 - 1982)</li>
+                <li>5. Prof. Ioan Crişan (1982 - 1988)</li>
+                <li>6. Prof. Iulian Raţiu (1988 - 1990)</li>
+                <li>7. Prof. Ioan Morar (1990 - 1991)</li>
+                <li>8. Prof. Titus Beşa-Fluieraş (1991 - 2001)</li>
+                <li>9. Traian Covrig</li>
+                <li>10. Ioan Moloca (2001 - 2004)</li>
+                <li>11. Marius Moldovan (2004 - 2009)</li>
+                <li>12. Vasile Moldovan (2009 - 2012)</li>
+                <li>13. Hatfaludi Alexandru (2013-now)</li>
+            </ul>
+
+       
+<p class="title">Cultural Attaches:</p>
+<ul>
+    <li>1. Prof. Ioan Bârsan (1974 - 1981)</li>
+    <li>2. Prof. Mîndru Alexandru (1981 - 2003)</li>
+    <li>3. Prof. Victor Bercea (1982 - 1989; 2002 - now)</li>
+    <li>4. Ing. Marius Moldovan (2004 - now)</li>
+    <li>5. Anghel Mirela (2003 - now)</li>
+</ul>
+
+<p class="title">Singers:</p>
+<ul>
+    <li>1. Mariana Chiorean</li>
+    <li>2. Ioan Dragoş</li>
+    <li>3. Ioan Fechete</li>
+    <li>4. Ionuţ Fulea</li>
+    <li>5. Marius Fulea</li>
+    <li>6. Georgeta Fârţală</li>
+    <li>7. Adriana Irimieş</li>
+    <li>8. Felicia Lupu</li>
+    <li>9. Dorina Pentelescu</li>
+    <li>10. Ioan Sîncu</li>
+    <li>11. Crina Varga</li>
+    <li>12. Rozalia Iuga</li>
+    <li>13. Dorina Grad</li>
+    <li>14. Maria Mariş</li>
+    <li>15. Simona Baciu</li>
+    <li>16. Anda Conţiş</li>
+    <li>17. Mircea Sime</li>
+    <li>18. Florin Bota</li>
+    <li>19. Silviu Adrian Popa</li>
+    <li>20. Irinel Popa</li>
+    <li>21. Mirela Suciu</li>
+    <li>22. Camelia Lupaş</li>
+    <li>23. Claudiu Ciotleuş</li>
+</ul>
+<p class="a">THE STAR OF THE SINGER GROUP, AND ARTISTIC COUNSELOR OF THE "Românaşul": IONUŢ FULEA</p>
+
+<p class="title">Collaborating singers:</p>
+<ul>
+    <li>1. Mia Dan</li>
+    <li>2. Dumitru Sopon</li>
+    <li>3. Maria Marcu</li>
+    <li>4. Sava Negrean Brudaşcu</li>
+    <li>5. Veta Biriş</li>
+    <li>6. Nicolae Furdui Iancu</li>
+    <li>7. Ion Pojar</li>
+    <li>8. Maria Lobont</li>
+    <li>9. Angela Nistor</li>
+    <li>10. Mariana Morcan</li>
+    <li>11. Maria Dan Golban</li>
+    <li>12. Marius Ciprian Pop</li>
+    <li>13. Adriana Hagău</li>
+    <li>14. Aurel Ciceoan</li>
+    <li>15. Maria Dan Golban</li>
+</ul>
+
+<p class="title">Collaborating musicians:</p>
+<ul>
+    <li>1. Dumitru Fărcaş</li>
+    <li>2. Gheorghe Zamfir</li>
+    <li>3. Gheorghe Palcu</li>
+    <li>4. Ioan Berci</li>
+    <li>5. Dan Gâdea</li>
+    <li>6. Constantin Istici</li>
+    <li>7. Mircea Câmpeanu</li>
+</ul>
+
+<p class="title">Show hosts:</p>
+<ul>
+    <li>1. Vania Atudorei</li>
+    <li>2. Victor Bercea</li>
+</ul>
     </div>
-
-    <div class="short-history-images-container">
-        <img src="images/gallery/pozeVechi/1978_nasaud.JPG" alt="Nasaud-1978" class="photo-1">
-        <img src="images/gallery/pozeVechi/1978_ceterasi.JPG" alt="Ceterasi-1978" class="photo-2">
-    </div>
-
-    <p>&nbsp;</p>
-    <p class="title">Members:</p>
-    <span>Students of the Technical University and other Universities of Cluj-Napoca: each year, more than 80 students participate in our activities. Dancers, singers and musicians. Conductors, choreographers, stage directors, referents, artistic counselors.</span>
-
-    <p>&nbsp;</p>
-    <p class="title">Choreographers:</p>
-    <span>
-        <ul type="circle">
-            <li>1. Bacsai Olga (1974 - 1975)</li>
-            <li>2. Onut Alexandru (1975 - 1976)</li>
-            <li>3. Miclaus Octavian (1976 - 1978)</li>
-            <li>4. Prof.Florin Ciobanu (1979 - 1994)</li>
-            <li>5. Ing.Viorel Roman (1994-now)</li>
-        </ul>
-    </span>
-
-    <p>&nbsp;</p>
-    <p class="title">Conductors:</p>
-    <span>1. Prof.Paul Stoica (1974 - 1975)<br>
-    2. Iuliu Hoca (1975 - 1976)<br>
-    3. Romică Râmbu (1975 - 1976)<br>
-    4. Ştefan Lustig (1976 - 1982)<br>
-    5. Prof.Ioan Crişan (1982 - 1988)<br>
-    6. Prof.Iulian Raţiu (1988 - 1990)<br>
-    7. Prof.Ioan Morar (1990 - 1991)<br>
-    8. Prof.Titus Beşa-Fluieraş (1991 - 2001)<br>
-    9. Traian Covrig<br>
-    10. Ioan Moloca (2001 - 2004)<br>
-    11. Marius Moldovan (2004 - 2009)<br>
-    12. Vasile Moldova (2009 - 2012)<br>
-    13. Hatfaludi Alexandru (2013-now)</span>
-
-    <p>&nbsp;</p>
-    <p class="title">Cultural Attaches:</p>
-    <span class="cultural-attaches-array">
-        <p class="cultural-attaches-item">1. Prof.Ioan Bârsan (1974 - 1981)</p>
-        <p class="cultural-attaches-item">2. Prof.Mîndru Alexandru (1981 - 2003)</p>
-        <p class="cultural-attaches-item">3. Prof.Victor Bercea (1982 - 1989; 2002 - now)</p>
-        <p class="cultural-attaches-item">4. Ing.Marius Moldovan (2004 - now)</p>
-        <p class="cultural-attaches-item">5. Anghel Mirela (2003 - now)</p>
-    </span>
-
-    <p>&nbsp;</p>
-    <p class="title">Singers:</p>
-    <span class="solisti-array">
-        <p class="solisti-item">1. Mariana Chiorean</p>
-        <p class="solisti-item">2. Ioan Dragoş</p>
-        <p class="solisti-item">3. Ioan Fechete</p>
-        <p class="solisti-item">4. Ionuţ Fulea</p>
-        <p class="solisti-item">5. Marius Fulea</p>
-        <p class="solisti-item">6. Georgeta Fârţală</p>
-        <p class="solisti-item">7. Adriana Irimieş</p>
-        <p class="solisti-item">8. Felicia Lupu</p>
-        <p class="solisti-item">9. Dorina Pentelescu</p>
-        <p class="solisti-item">10. Ioan Sîncu</p>
-        <p class="solisti-item">11. Crina Varga</p>
-        <p class="solisti-item">12. Rozalia Iuga</p>
-        <p class="solisti-item">13. Dorina Grad</p>
-        <p class="solisti-item">14. Maria Mariş</p>
-        <p class="solisti-item">15. Simona Baciu</p>
-        <p class="solisti-item">16. Anda Conţiş</p>
-        <p class="solisti-item">17. Mircea Sime</p>
-        <p class="solisti-item">18. Florin Bota</p>
-        <p class="solisti-item">19. Silviu Adrian Popa</p>
-        <p class="solisti-item">20. Irinel Popa</p>
-        <p class="solisti-item">21. Mirela Suciu</p>
-        <p class="solisti-item">22. Camelia Lupaş</p>
-        <p class="solisti-item">23. Claudiu Ciotleuş</p>
-    </span>
-    <p class="a">THE STAR OF THE SINGER GROUP, AND ARTISTIC COUNSELOR OF THE &quot;Românaşul&quot;: IONUŢ FULEA</p>
-
-    <p>&nbsp;</p>
-    <p class="title">Collaborating singers:</p>
-    <span>
-        1. Mia Dan<br>
-        2. Dumitru Sopon<br>
-        3. Maria Marcu<br>
-        4. Sava Negrean Brudaşcu<br>
-        5. Veta Biriş<br>
-        6. Nicolae Furdui Iancu<br>
-        7. Ion Pojar<br>
-        8. Maria Lobont<br>
-        9. Angela Nistor<br>
-        10. Mariana Morcan<br>
-        11. Maria Dan Golban<br>
-        12. Marius Ciprian Pop<br>
-        13. Adriana Hagău<br>
-        14. Aurel Ciceoan<br>
-        15. Maria Dan Golban
-    </span>
-
-    <p>&nbsp;</p>
-    <p class="title">Collaborating musicians:</p>
-    <span>
-        1. Dumitru Fărcaş<br>
-        2. Gheorghe Zamfir<br>
-        3. Gheorghe Palcu<br>
-        4. Ioan Berci<br>
-        5. Dan Gâdea<br>
-        6. Constantin Istici<br>
-        7. Mircea Câmpeanu
-    </span>
-
-    <p>&nbsp;</p>
-    <p class="title">Show hosts:</p>
-    <span>1. Vania Atudorei<br>
-        2. Victor Bercea
-    </span>
-
     </div>
 </body>
 
