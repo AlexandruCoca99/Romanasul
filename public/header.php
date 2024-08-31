210aa4d663259ba8ad595cbf4e4bbd5f7f8c4f2e
AlexandruCoca99 <114983771+AlexandruCoca99@users.noreply.github.com>
2024-08-31 16:45:31 +0300

    navbar responsive

--- public/header.php
+++ public/header.php
@@ -1,55 +1,18 @@
 <header>
     <!-- <div class="border"> -->
+    <div class="hamburger">
+        <i class="fa-solid fa-bars"></i>
+    </div>
     <nav>
-        <a href="home.php" class="scrollBtn <?php if ($page == 'home')
-                                                echo 'selected'; ?>">
-            <?php echo _("Home") ?>
-        </a> |
-
-        <!-- stilizat fain -->
-        <a href="whoweare.php" class="scrollBtn <?php if ($page == 'whoweare')
-                                                    echo 'selected'; ?>">
-            <?php echo _("Who we are") ?>
-        </a> |
-
-        <!-- galerie impartita pe perioade de ani -->
-        <a href="images.php" class="scrollBtn <?php if ($page == 'images')
-                                                    echo 'selected'; ?>">
-            <?php echo _("Images") ?>
-        </a> |
-        <!-- 3 sectiuni -> playlist muzica, carusel videoclipuri, carusel spectacole -->
-        <a href="tours.php" class="scrollBtn <?php if ($page == 'tours')
-                                                    echo 'selected'; ?>">
-            <?php echo _("Tours") ?>
-        </a> |
-        <!-- harta (festivaluri) -->
-        <a href="../competitions.php" class="scrollBtn <?php if ($page == 'competitions')
-                                                            echo 'selected'; ?>">
-            <?php echo _("Competitions") ?>
-        </a> |
-
-        <!-- pt Vincze si EU -->
-        <!-- Evenimente/Events (dropdown -> nunti/majorate, petreceri, spectacole, corporate events ) -->
-        <!-- galerie cu poze de la fiecare tip de eveniment cu float  de contact -->
-
-        <a href="repertoriu.php" class="scrollBtn <?php if ($page == 'repertoriu')
-                                                        echo 'selected'; ?>">
-            <?php echo _("Repertoriu") ?>
-        </a> |
-
-        <a href="reviews.view.php" class="scrollBtn <?php if ($page == 'reviews')
-                                                        echo 'selected'; ?>">
-            <?php echo _("Reviews") ?>
-        </a> |
-
-        <!-- Pt Nenea Luci -->
-        <a href="contacts.php" class="scrollBtn <?php if ($page == 'contacts')
-                                                    echo 'selected'; ?>">
-            <?php echo _("Contact Us") ?>
-        </a>
-
-
-
+        <ul>
+            <li> <a href="home.php" class="scrollBtn <?php if ($page == 'home') echo 'selected'; ?>"><?php echo _("Home") ?></a></li>
+            <li> <a href="whoweare.php" class="scrollBtn <?php if ($page == 'whoweare') echo 'selected'; ?>"><?php echo _("Who we are") ?></a></li>
+            <li> <a href="images.php" class="scrollBtn <?php if ($page == 'images') echo 'selected'; ?>"><?php echo _("Images") ?></a></li>
+            <li> <a href="tours.php" class="scrollBtn <?php if ($page == 'tours') echo 'selected'; ?>"><?php echo _("Tours") ?></a></li>
+            <li> <a href="repertoriu.php" class="scrollBtn <?php if ($page == 'repertoriu') echo 'selected'; ?>"><?php echo _("Repertoriu") ?></a></li>
+            <li> <a href="reviews.view.php" class="scrollBtn <?php if ($page == 'reviews') echo 'selected'; ?>"><?php echo _("Reviews") ?></a></li>
+            <li> <a href="contacts.php" class="scrollBtn <?php if ($page == 'contacts') echo 'selected'; ?>"><?php echo _("Contact Us") ?></a></li>
+        </ul>
     </nav>
     <div class="logo scrollBtn" title="Romanasul Cluj-Napoca"></div>
     <!-- </div> -->
--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,53 +1,59 @@
-new Swiper(".swiper-homepage", {
-  effect: "fade",
-  autoHeight: true,
-  centeredSlides: true,
-  spaceBetween: 24,
-  grabCursor: true,
-  pagination: {
-    el: ".swiper-homepage .swiper-custom-pagination",
-    clickable: true,
-    renderBullet: function (index, className) {
-      return `<div class=${className}>
+jQuery(document).ready(function ($) {
+  $(document).on("click", ".hamburger ", function () {
+    $("nav").toggleClass("active");
+  });
+
+  new Swiper(".swiper-homepage", {
+    effect: "fade",
+    autoHeight: true,
+    centeredSlides: true,
+    spaceBetween: 24,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-homepage .swiper-custom-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return `<div class=${className}>
                 <span class="number">${index + 1}</span>
                 <span class="line"></span>
                 </div>`;
+      },
     },
-  },
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  lazyLoading: true,
-  loop: true,
-  keyboard: {
-    enabled: true,
-  },
-  navigation: {
-    nextEl: "#nav-right",
-    prevEl: "#nav-left",
-  },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    lazyLoading: true,
+    loop: true,
+    keyboard: {
+      enabled: true,
+    },
+    navigation: {
+      nextEl: "#nav-right",
+      prevEl: "#nav-left",
+    },
+  });
+
+  const countdown = () => {
+    const countDate = new Date("November 17, 2024 20:00:00").getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+    document.getElementById("days").innerText = textDay;
+    document.getElementById("hours").innerText = textHour;
+    document.getElementById("minutes").innerText = textMinute;
+    document.getElementById("seconds").innerText = textSecond;
+  };
+
+  setInterval(countdown, 1000);
 });
-
-const countdown = () => {
-  const countDate = new Date("November 17, 2024 20:00:00").getTime();
-  const now = new Date().getTime();
-  const gap = countDate - now;
-
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const textDay = Math.floor(gap / day);
-  const textHour = Math.floor((gap % day) / hour);
-  const textMinute = Math.floor((gap % hour) / minute);
-  const textSecond = Math.floor((gap % minute) / second);
-
-  document.getElementById("days").innerText = textDay;
-  document.getElementById("hours").innerText = textHour;
-  document.getElementById("minutes").innerText = textMinute;
-  document.getElementById("seconds").innerText = textSecond;
-};
-
-setInterval(countdown, 1000);
